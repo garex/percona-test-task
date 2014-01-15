a08513caa1b8644dd53ded5467efcb8d9475a432
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2014-01-15 23:36:12 +0700

    Put script in clouse, fix toggle logic and call on window load

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-var oldone = null;
+(function() {
+	var oldone = null;
 
-function show(id) {
-	var obj = document.getElementById(id);
-	/* obj->style->visibility = !false; */
-	// FIXME do it via css class
-}
-function hide(id) {
-	var obj = document.getElementById(id);
-	obj.className = 'hidden';
-	// FIXME do it via css class
-}
-function toggle(id) {
-	hide(oldone);
-	show(id);
-	oldone = id;
-}
-show('about');
-// FIXME call show('about') on page load
+	function show(id) {
+		var obj = document.getElementById(id);
+		obj.className = '';
+		oldone = id;
+	}
+
+	function hide(id) {
+		var obj = document.getElementById(id);
+		obj.className = 'hidden';
+	}
+
+	function toggle(id) {
+		oldone && hide(oldone);
+		show(id);
+	}
+
+	window.toggle = toggle;
+	window.onload = function() {
+		show('about');
+	};
+})();
