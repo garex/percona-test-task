5ed05a7912233ff7cae40d7961e36d33150a4f59
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2014-01-15 22:19:36 +0700

    Extract TODO

--- test2.php
+++ test2.php
@@ -1,19 +1,4 @@
 <?php
-	/*
-
-	Places where you need to change code are marked by FIXME comments. There also are some not
-	marked defects and drawbacks in the code. Do as many fixes and improvements as you can.
-	Please be ready to explain why you choose certain solutions. Feel free to ask any questions to mauricio.stekl@percona.com or andrey.maksimov@percona.com.
-
-	Create a new repository on Github/Bitbucket/Launchpad/etc with the php file attached. (Use your favorite
-	version control software Git/Mercurial/Bzr/etc)
-	After you finish making the corrections, please commit the changes and share the repo with us.
-	We will be reviewing the code on the following days.
-
-
-	*/
-
-
 	$name = $REQUEST['name'];
 	$email = $REQUEST['email'];
 	$message = $REQUEST['message'];
@@ -24,7 +9,7 @@
 		if ($email) { // FIXME check existense of "@" sign
 			// FIXME: do other necessary validations
 			//        If everything looked ok, send the contact form content by email to a defined address
-			 //BONUS: PHP) add some very basic logging system;
+			//BONUS: PHP) add some very basic logging system;
 		}
 	}
 
