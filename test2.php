872c31ba1c4ac80a7f8eb2bed591446acb2cb51a
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2014-01-15 22:59:33 +0700

    Making XHTML strict complied
    * Tnx to http://validator.w3.org/check

--- test2.php
+++ test2.php
@@ -7,59 +7,63 @@
 		if ($email) { // FIXME check existense of "@" sign
 			// FIXME: do other necessary validations
 			//        If everything looked ok, send the contact form content by email to a defined address
-			//BONUS: PHP) add some very basic logging system;
+			// BONUS: PHP) add some very basic logging system;
 		}
 	}
 ?>
-<!-- // ensure STRICT XHTML compliance -->
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Task for Hire</title>
 	<link href="style.css" media="all" rel="stylesheet" type="text/css" />
 </head>
 <body>
-	<?php   //  FIXME: Make this title change when switching between sections
-			//  BONUS: CSS) use a nice font for this title.
-	?>
+	<?php // FIXME: Make this title change when switching between sections ?>
+	<?php // BONUS: CSS) use a nice font for this title ?>
 	<h1>Example</h1>
+
+	<?php // BONUS: CSS) make this an horizontal menu with rounded buttons ?>
+	<?php // BONUS: JS) make this an horizontal menu with rounded buttons ?>
 	<div>
-<?php
-	// BONUS: CSS) make this an horizontal menu with rounded buttons ; JS) make the transition between sections look really nice
-	echo "<ul>";
-	print '<li><a onclick="javascript:void(toggle("about"))" href="#about">about</a>';
-	print '<li><a onclick="javascript:void(toggle("services"))" href="#services">services</a>';
-	print '<li><a onclick="javascript:void(toggle("customers"))" href="#customers">customers</a>';
-	print '<li><a onclick="javascript:void(toggle("contacts"))" href="#contacts">contacts</a>';
-	echo "</ul>";
-?>
-	</div><br><br><br>
+		<ul>
+			<li><a onclick="javascript:void(toggle('about'))" href="#about">about</a></li>
+			<li><a onclick="javascript:void(toggle('services'))" href="#services">services</a></li>
+			<li><a onclick="javascript:void(toggle('customers'))" href="#customers">customers</a></li>
+			<li><a onclick="javascript:void(toggle('contacts'))" href="#contacts">contacts</a></li>
+		</ul>
+	</div>
 
-<?php
-	// BONUS: DESIGN/CSS) improve how the section contents looks
-	echo '<div id="about" class="hidden"><!-- // -></div>';
-	echo '<div id="services" class="hidden"><!- // --></div>';
-	echo '<div id="customers" class="hidden"><!-- // -></div>';
-	echo '<div id="contacts" class="hidden"><!- // --></div>';
-?>
+	<p>&nbsp;</p>
 
-<?php
+	<?php // BONUS: DESIGN/CSS) improve how the section contents looks ?>
+	<div id="about" class="hidden">
+		<!-- // -->
+	</div>
+	<div id="services" class="hidden">
+		<!-- // -->
+	</div>
+	<div id="customers" class="hidden">
+		<!-- // -->
+	</div>
+	<div id="contacts" class="hidden">
+		<!-- // -->
+	</div>
+
+	<?php
 	if (false) {
-	// FIXME when url === #contacts,
-	// Display a form with three parameters:
-			// - Name (Text Input, 25 chars max),
-			// - Email(Text Input, 25 chars with obligatory @ sign),
-			// - Message (Text Area, 255 chars)
-	// Add simple antispam protection (anyone you propose // please explain why)
-	// If parameters are wrong, the form must be pre-populated with submitted data
-	// BONUS: JS/CSS) make the form look awesome
-
-		print '
-		';
+		// FIXME when url === #contacts,
+		// Display a form with three parameters:
+				// - Name (Text Input, 25 chars max),
+				// - Email(Text Input, 25 chars with obligatory @ sign),
+				// - Message (Text Area, 255 chars)
+		// Add simple antispam protection (anyone you propose // please explain why)
+		// If parameters are wrong, the form must be pre-populated with submitted data
+		// BONUS: JS/CSS) make the form look awesome
 	}
-?>
+	?>
 
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script src="script.js"></script>
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script type="text/javascript" src="script.js"></script>
 </body>
 </html>
